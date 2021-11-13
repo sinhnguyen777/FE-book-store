35056fde5aaca8c37612c26a037e276133e69f5f
công nguyễn đức <duccongnguyen14102001@gmail.com>
2021-11-13 15:40:01 +0700

    update folder

---
+++ src/Page/Admin/Page/Home/Home.jsx
@@ -0,0 +1,22 @@
+import { Menu, Button } from 'antd';
+
+import {
+    AppstoreOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    PieChartOutlined,
+    DesktopOutlined,
+    ContainerOutlined,
+    MailOutlined,
+} from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+const Header = () => {
+
+    return (
+        <></>
+    );
+}
+
+export default Header;

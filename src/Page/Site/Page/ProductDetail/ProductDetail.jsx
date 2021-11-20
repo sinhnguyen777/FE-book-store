c26d3ec2b28c9f9f77130a753edb141d45b9e32c
sinhnguyen777 <sinhnt990@gmail.com>
2021-11-20 21:21:59 +0700

    fixbugs product_detail product_meta

--- src/Page/Site/Page/ProductDetail/ProductDetail.jsx
+++ src/Page/Site/Page/ProductDetail/ProductDetail.jsx
@@ -7,7 +7,7 @@ export default function ProductDetail() {
     const [visible, setVisible] = useState(false);
 
     return (
-        <div style={{width: '100%'}}>
+        <div style={{ width: '100%' }}>
             <div className="banner_product_detail">
                 <div className="title_banner_product">
                     <h6>PRODUCTS</h6>
@@ -32,15 +32,44 @@ export default function ProductDetail() {
 
                         </div>
                     </Col>
-                    <Col span={16} style={{padding: '0 0 0 22px'}}>
+                    <Col span={16} style={{ padding: '0 0 0 22px' }}>
                         <div className="product_detail">
                             <div className="title_author">
-                                <Link to="/author">BY JAMES HOFFMAN</Link>
+                                <Link to="/author">by james hoffman</Link>
                             </div>
                             <h2 className="title_product_detail">Amster Hamster Trip</h2>
                             <div className="rating_product_detail">
-                                <Rate style={{color: '#d14031'}} disabled defaultValue={5} />
+                                <Rate style={{ color: '#d14031' }} disabled defaultValue={5} />
                             </div>
+                            <p className="price_product_detail">$38.00</p>
+                            <div className="description_product_detail">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit. Simul vidisse eu vim. Probo tincidunt ne vel.
+                                </p>
+                            </div>
+                            <p className="out_of_stock">Out of stock</p>
+                            <div className="product_meta">
+                                <span>SKU: $39</span>
+                                <p>
+                                    <span>
+                                        Categories:
+                                        <Link to=""> Art</Link>
+                                        ,
+                                        <Link to=""> Best Sellers</Link>
+                                        ,
+                                        <Link to=""> History</Link>
+                                    </span>
+                                </p>
+                                <p>
+                                    <span>
+                                        Tags:
+                                        <Link to=""> Bestseller</Link>
+                                        ,
+                                        <Link to=""> Fiction</Link>
+                                    </span>
+                                </p>
+                            </div>
+                            
                         </div>
                     </Col>
                 </Row>

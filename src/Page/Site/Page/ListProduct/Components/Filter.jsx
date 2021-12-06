b7dcccaa564f4fb0ea6f00e5f566a29fa46b7c5d
công nguyễn đức <duccongnguyen14102001@gmail.com>
2021-12-06 23:22:20 +0700

    fix Dev-cong

--- src/Page/Site/Page/ListProduct/Components/Filter.jsx
+++ src/Page/Site/Page/ListProduct/Components/Filter.jsx
@@ -4,8 +4,11 @@ import { Slider } from 'antd';
 import { Link } from 'react-router-dom';
 import cataApi from '../../../../../api/cataApi';
 import { LoadingOutlined } from '@ant-design/icons';
-const Filter = () => {
+
+const Filter = (props) => {
     const [catalog, setCatalog] = useState([])
+    const [minPrice, setminPrice] = useState(0);
+    const [maxPrice, setmaxPrice] = useState(500000);
     useEffect(() => {
         const fetchCata = async () => {
             const res = await cataApi.GetCata()
@@ -14,61 +17,33 @@ const Filter = () => {
         fetchCata()
     }, [])
     console.log(catalog);
+
+    const handlePrice = (values) => {
+        console.log(values);
+        setminPrice(values[0]);
+        setmaxPrice(values[1])
+    }
     return (
         <>
             <div className="Filter-search">
-                <InputSearch/>
+                <InputSearch />
             </div>
             <div className="Filter-price">
                 <div className="Filter-price_title">Lọc theo giá</div>
                 <form action="">
                     <div className="Filter-price_slider">
-                        <Slider range={{ draggableTrack: true }} defaultValue={[0, 100]} />
+                        <Slider range={{ draggableTrack: true }} defaultValue={[0, 500000]} onChange={handlePrice} max={500000} step={1000} />
                         <div className="Filter-price_amount">
-                            <div className="Filter-price_amount_label">Giá: 
-                                <span className="Filter-price_amount_from"> 0đ</span> —
-                                <span className="Filter-price_amount_to"> 100đ</span>
+                            <div className="Filter-price_amount_label">Giá:
+                                <span className="Filter-price_amount_from"> {minPrice}đ</span> —
+                                <span className="Filter-price_amount_to"> {maxPrice}đ</span>
                             </div>
                             <button className="Filter-price_amount_button">Lọc</button>
                         </div>
                     </div>
                 </form>
             </div>
-            <div className="Filter-line">
-                <span className="Filter-line_left Filter-line_line"></span>
-                <div className="Filter-line_icon">❦</div>
-                <div className="Filter-line_right Filter-line_line"></div>
-            </div>
-            <div className="Filter-product-top">
-                <div className="Filter-product-top_title">Top 5 trong tuần</div>
-                <div className="Filter-product-top_list">
-                    <div className="Filter-product-top_list_image">
-                        <Link to='/product-detail'>
-                            <img src="https://chapterone.qodeinteractive.com/wp-content/uploads/2019/07/product-4-741x1024.jpg" alt="" />
-                        </Link>
-                    </div>
-                    <div className="Filter-product-top_list_image">
-                        <Link to='/product-detail'>
-                            <img src="https://chapterone.qodeinteractive.com/wp-content/uploads/2019/07/product-4-741x1024.jpg" alt="" />
-                        </Link>
-                    </div>
-                    <div className="Filter-product-top_list_image">
-                        <Link to='/product-detail'>
-                            <img src="https://chapterone.qodeinteractive.com/wp-content/uploads/2019/07/product-4-741x1024.jpg" alt="" />
-                        </Link>
-                    </div>
-                    <div className="Filter-product-top_list_image">
-                        <Link to='/product-detail'>
-                            <img src="https://chapterone.qodeinteractive.com/wp-content/uploads/2019/07/product-4-741x1024.jpg" alt="" />
-                        </Link>
-                    </div>
-                    <div className="Filter-product-top_list_image">
-                        <Link to='/product-detail'>
-                            <img src="https://chapterone.qodeinteractive.com/wp-content/uploads/2019/07/product-4-741x1024.jpg" alt="" />
-                        </Link>
-                    </div>
-                </div>
-            </div>
+
             <div className="Filter-line">
                 <span className="Filter-line_left Filter-line_line"></span>
                 <div className="Filter-line_icon">❦</div>
@@ -77,13 +52,16 @@ const Filter = () => {
             <div className="Filter-catalog">
                 <div className="Filter-catalog_title">Danh mục</div>
                 <div className="Filter-catalog_list">
+                    <p>
+                        <span onClick={() => props.handleListCata(null)}>Tất cả</span>
+                    </p>
                     {
                         catalog.data ?
-                        catalog.data.map((item,index) => ( 
-                            <p key={index}>
-                                <Link to={`/product-category/${item._id}`}>{item.nameCata}</Link>
-                            </p>
-                        )) : <LoadingOutlined/>
+                            catalog.data.map((item, index) => (
+                                <p key={index}>
+                                    <span onClick={() => props.handleListCata(item._id)}>{item.nameCata}</span>
+                                </p>
+                            )) : <LoadingOutlined />
                     }
                 </div>
             </div>

4a0379509f579223bbda560dcc6cb24d607b275c
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-12-07 18:53:18 +0700

    service

--- src/Page/Site/Page/Option/Option.jsx
+++ src/Page/Site/Page/Option/Option.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ItemOption from './Components/ItemOption'
 
 const Option = () => {
     return (
@@ -7,6 +8,15 @@ const Option = () => {
                 <div className="option_banner_title">
                     <h6>info</h6>
                     <h2>Dịch vụ</h2>
+                </div>
+            </div>
+            <div className="layout">
+                <div className="Option-text">
+                    <h4>Tùy chọn giá cả</h4>
+                    <h2>Sách trực tuyến mới nhất</h2>
+                </div>
+                <div className="Option-content">
+                    <ItemOption/>
                 </div>
             </div>
         </>

6c6075a975ca906e82844a31c76513ba42a335f3
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-12-17 00:22:56 +0700

    Update axiosCilentForm.js

--- src/api/axiosCilentForm.js
+++ src/api/axiosCilentForm.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClientForm = axios.create({
   baseURL:
-    "http://localhost:5000" /* https://beonlinelibrary.herokuapp.com http://localhost:5000' */,
+    "https://beonlinelibrary.herokuapp.com " /* https://beonlinelibrary.herokuapp.com http://localhost:5000' */,
   headers: { "Content-Type": "multipart/form-data" },
 });
 

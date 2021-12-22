e61c89e9a5d7c26c4f3f36043ac5478bdb7d6142
sinhnguyen777 <sinhnt990@gmail.com>
2021-12-22 17:23:48 +0700

    page congratulation

--- src/api/axiosCilent.js
+++ src/api/axiosCilent.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://beonlinelibrary.herokuapp.com" /* https://beonlinelibrary.herokuapp.com *//*  http://localhost:5000 */,
+  baseURL: "http://localhost:5000" /* https://beonlinelibrary.herokuapp.com *//*  http://localhost:5000 */,
   headers: {
     "Content-Type": "application/json",
   },

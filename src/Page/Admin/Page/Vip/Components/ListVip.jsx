5fe2d663887d67004a88776f5e2249bf90300db2
hothilan0101 <90611249+hothilanstm01@users.noreply.github.com>
2021-12-22 19:29:02 +0700

    Update ListVip.jsx

--- src/Page/Admin/Page/Vip/Components/ListVip.jsx
+++ src/Page/Admin/Page/Vip/Components/ListVip.jsx
@@ -1,51 +1,38 @@
-import { Button, Table } from "antd";
+import { Button, Table } from 'antd';
 import React from "react";
 import { Link } from "react-router-dom";
 
 const ListVip = (props) => {
   const columns = [
     {
-      title: "Gói VIP",
-      dataIndex: "name",
-      key: "name",
+      title: 'Gói VIP',
+      dataIndex: 'name',
+      key: 'name',
     },
     {
-      title: "Giá gói",
-      key: "price",
+      title: 'Giá gói',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Thời gian hết hạn',
+      dataIndex: 'time',
+      key: 'time',
+    },
+    {
+      title: 'Sửa',
+      key: 'edit',
       render: (record) => {
-        return (
-          <>
-            {record.price.toLocaleString("vi", {
-              style: "currency",
-              currency: "VND",
-            })}
-          </>
-        );
+        return <>
+          <Button><Link to={`/admin/vip/edit/${record._id}`}>Sửa</Link></Button>
+        </>
       },
     },
     {
-      title: "Thời gian hết hạn",
-      dataIndex: "time",
-      key: "time",
-    },
-    {
-      title: "Sửa",
-      key: "edit",
-      render: (record) => {
-        return (
-          <>
-            <Button>
-              <Link to={`/admin/vip/edit/${record._id}`}>Sửa</Link>
-            </Button>
-          </>
-        );
-      },
-    },
-    {
-      title: "Xóa",
-      key: "del",
+      title: 'Xóa',
+      key: 'del',
       render: (record) => (
-        <Button onClick={() => props.handleRemove(record._id)}>Xóa</Button>
+        <Button onClick={()=>props.handleRemove(record._id)}>Xóa</Button>
       ),
     },
   ];
@@ -55,6 +42,6 @@ const ListVip = (props) => {
       <Table dataSource={props.data} columns={columns} />
     </>
   );
-};
+}
 
-export default ListVip;
+export default ListVip

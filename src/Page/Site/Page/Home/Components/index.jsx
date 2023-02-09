2fb33a7df6f8c44ec844a0a19390ca9473753140
sinhnguyen <sinhnguyen.dev@gmail.com>
2023-02-09 15:43:30 +0700

    fix: build

--- src/Page/Site/Page/Home/Components/index.jsx
+++ src/Page/Site/Page/Home/Components/index.jsx
@@ -1,8 +1,8 @@
-export { default as Autobiography } from "./Autobiography";
-export { default as AuthorComponent } from "./AuthorComponent";
-export { default as ListBestSeller } from "./ListBestSeller";
-export { default as ListHot } from "./ListHot";
-export { default as ListNew } from "./ListNew";
-export { default as ListSale } from "./ListSale";
-export { default as ParallaxImg } from "./ParallaxImg";
-export { default as PartnerComponent } from "./PartnerComponent";
+export { default as Autobiography } from "./Autobiography.jsx";
+export { default as AuthorComponent } from "./AuthorComponent.jsx";
+export { default as ListBestSeller } from "./ListBestSeller.jsx";
+export { default as ListHot } from "./ListHot.jsx";
+export { default as ListNew } from "./ListNew.jsx";
+export { default as ListSale } from "./ListSale.jsx";
+export { default as ParallaxImg } from "./ParallaxImg.jsx";
+export { default as PartnerComponent } from "./PartnerComponent.jsx";

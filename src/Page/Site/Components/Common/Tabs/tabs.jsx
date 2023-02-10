9b435c1b50789e9664ba3c5891af4e1936deefb4
sinhnguyen <sinhnguyen.dev@gmail.com>
2023-02-11 01:08:43 +0700

    fix: build

--- src/Page/Site/Components/Common/Tabs/tabs.jsx
+++ src/Page/Site/Components/Common/Tabs/tabs.jsx
@@ -1,12 +1,88 @@
 import { Tabs } from 'antd';
-import React from 'react';
-import ListHot from '../../../Page/Home/components/ListHot';
-import ListNew from '../../../Page/Home/components/ListNew';
-import ListSale from '../../../Page/Home/components/ListSale';
+import React, { useEffect, useState } from 'react';
+import prouctApi from '../../../../../api/productApi';
+import ItemProduct from '../../../Page/ListProduct/Components/ItemProduct';
 
 
+const ListHot = () => {
+    const [DataHot, setDataHot] = useState([]);
+
+    useEffect(() => {
+        const filter = {
+            productHot: true
+        };
+        const fetchProductHot = async (filter) => {
+
+            const res = await prouctApi.GetProducts(filter);
+            setDataHot(res.data);
+        };
+
+        fetchProductHot(filter);
+    }, []);
+
+    const data = DataHot.slice(0, 5);
+    // console.log(data);
+    return (
+        <div className="List-product">
+            <ItemProduct data={data} />
+
+        </div>
+    );
+};
+
+const ListNew = () => {
+    const [productNew, setProductNew] = useState([]);
+    useEffect(() => {
+        const filter = {
+            _sort: "",
+            column: "createdAt",
+            type: "desc",
+        };
+        const fetchProductHot = async (filter) => {
+            const res = await prouctApi.GetProducts(filter);
+            setProductNew(res.data);
+        };
+
+        fetchProductHot(filter);
+    }, []);
+
+    const data = productNew.slice(0, 5);
+    console.log(data);
+    return (
+        <div className="List-product">
+            <ItemProduct data={data} />
+        </div>
+    );
+};
+
+const ListSale = () => {
+    const [DataHot, setDataHot] = useState([]);
+
+    useEffect(() => {
+        const filter = {
+            productSale: true
+        };
+        const fetchProductHot = async (filter) => {
+
+            const res = await prouctApi.GetProducts(filter);
+            setDataHot(res.data);
+        };
+
+        fetchProductHot(filter);
+    }, []);
+
+    const data = DataHot.slice(0, 5);
+    console.log(data);
+    return (
+        <div className="List-product">
+            <ItemProduct data={data} />
+
+        </div>
+    );
+};
 
 const { TabPane } = Tabs;
+
 const Tab = () => {
     return (
         <Tabs defaultActiveKey="1" centered>
